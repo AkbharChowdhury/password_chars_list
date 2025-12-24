31cfa84ccf080d19836eaf63f1f79a64ada9cb15
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-12-24 11:00:36 +0000

    a

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import os
 from typing import Generator
 from dotenv import load_dotenv
+
 load_dotenv()
 
-def get_password_chars_list(chars_index:list[int]) -> Generator[str]:
+
+def get_password_chars_list(chars_index: list[int]) -> Generator[str]:
     my_password: str = os.getenv('PASSWORD')
-    password_chars: Generator[str] = (my_password[i - 1] for i in chars_index)
-    return password_chars
+    return (my_password[i - 1] for i in chars_index)
+
+
 def main():
-    chars_index_list: list[str] = list(get_password_chars_list(chars_index=[1, 2, 3]))
-    print(list(chars_index_list))
+    chars_index_list: list[str] = list(get_password_chars_list(chars_index=[2, 4, 11]))
+    print(chars_index_list)
+
 
 if __name__ == '__main__':
     main()
